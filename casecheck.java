a69dffbce8506c12a01765f9bdd814a3132788e0
yoyouuff <hardilshukla@gmail.com>
2025-12-30 11:29:46 +0000

    Update

---
+++ casecheck.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class casecheck {
+    public static void main(String[] args) {
+
+    // Creating a simple code to check whether the first letter of given word is upper caes or lower case.
+
+        System.out.print("Enter your word:");
+        Scanner input = new Scanner(System.in);
+        char ch = input.next().trim().charAt(0);
+       
+        if(ch >='a'&& ch <= 'z' ) {
+            System.out.println("The first letter is lower  case.");
+        }
+        else{
+            System.out.println("The first letter is Upper case.");
+        }
+
+    }
+
+}
