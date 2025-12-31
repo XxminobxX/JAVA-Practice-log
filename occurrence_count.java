93457376634c2218027cc94ca51b12c606d5f919
yoyouuff <hardilshukla@gmail.com>
2025-12-31 16:44:38 +0000

    update

--- occurrence_count.java
+++ occurrence_count.java
@@ -5,20 +5,30 @@ import java.util.Scanner;
 public class occurrence_count{
     public static void main(String[] args) {
 
-        System.out.println("Enter a long number:");
+        System.out.print("Enter a long number:");
         Scanner sc = new Scanner(System.in);
-        long num =  sc.nextLong();
+        int num =  sc.nextInt();  
 
-        System.out.println("Enter the number whose occurrence you want to check:");
+        System.out.print("Enter the number whose occurrence you want to check:");
         Scanner sc1 = new Scanner(System.in);
-        
+        int check = sc1.nextInt();
 
-        
+        int occur = 0;
 
+        while (num > 0){
+              int rem = num % 10;
             
+               if (rem==check){
+                occur++;
+             }
+            
+             num/=10;
+            
+             
+         }
+
+      System.out.println("The number have appeared this many times:" +occur);
+        
     }
  
-
-
-
 }
