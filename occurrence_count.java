6d8211864ccd7f99f91fa4fd62d57ca8a5a0ab8a
yoyouuff <hardilshukla@gmail.com>
2025-12-31 15:52:33 +0000

    update

---
+++ occurrence_count.java
@@ -0,0 +1,24 @@
+//Creating a program that check the occurrence of a particular number from a user.
+
+import java.util.Scanner;
+
+public class occurrence_count{
+    public static void main(String[] args) {
+
+        System.out.println("Enter a long number:");
+        Scanner sc = new Scanner(System.in);
+        long num =  sc.nextLong();
+
+        System.out.println("Enter the number whose occurrence you want to check:");
+        Scanner sc1 = new Scanner(System.in);
+        
+
+        
+
+            
+    }
+ 
+
+
+
+}
