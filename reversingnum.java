e2b49632e13474377e4eed8752af0a1043239943
yoyouuff <hardilshukla@gmail.com>
2026-01-01 17:36:31 +0000

    update

---
+++ reversingnum.java
@@ -0,0 +1,21 @@
+// Wrtie a program to  reverse a number given by the user.
+
+import java.util.Scanner;
+
+public class reversingnum {
+    public static void main(String[] args) {
+
+        System.out.println("Enter the number you want to reverse:");
+        Scanner sc = new Scanner(System.in);
+        int user = sc.nextInt();
+        int ans = 0;
+
+        while (user>0) {
+            user %= 10;
+            
+            
+        }
+        
+
+    }
+}
