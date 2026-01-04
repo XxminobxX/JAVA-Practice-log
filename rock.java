656d215ba228b1c9e08cef1049346a8ca6d7af79
yoyouuff <hardilshukla@gmail.com>
2026-01-04 18:38:05 +0000

    Update

--- rock.java
+++ rock.java
@@ -2,12 +2,14 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class  rock {
+public class rock {
     public static void main(String[] args) {
-        Scanner sc  = new Scanner(System.in);
 
-        // Defining ASCII variables 
-         String rock = """
+        while (true) {
+            Scanner sc  = new Scanner(System.in);
+
+            // Defining ASCII variables 
+            String rock = """
     _______
 ---'   ____)
       (_____)
@@ -16,7 +18,7 @@ public class  rock {
 ---.__(___)
 """;
 
-String paper = """
+            String paper = """
      _______
 ---'    ____)____
            ______)
@@ -25,7 +27,7 @@ String paper = """
 ---.__________)
 """;
 
-String scissors = """
+            String scissors = """
     _______
 ---'   ____)____
           ______)
@@ -33,58 +35,87 @@ String scissors = """
       (____)
 ---.__(___)
 """;
-//Welcome message and how to play.
-System.out.println("Welcome to Rock,Paper,Scissors game.");
-System.out.println("It's YOU VS COMPUTER.");
-System.out.println(" Rock --> 0 \n Paper --> 1 \n Scissor --> 2 \n EXIT --> 9");
-System.out.print("Enter your choice: ");
-// Entering about user choice.
- int user = sc.nextInt();
+            //Welcome message and how to play.
+            System.out.println("Welcome to Rock,Paper,Scissors game.");
+            System.out.println("It's YOU VS COMPUTER.");
+            System.out.println(" Rock --> 0 \n Paper --> 1 \n Scissor --> 2 \n EXIT --> 9");
+            System.out.print("Enter your choice: ");
 
+            // Entering about user choice.
+            int user;
+            try {
+                user = sc.nextInt();
+            } catch (Exception e) {
+                System.out.println("Invalid input! Please enter numbers only.\n\n");
+                continue;
+            }
 
-// To generate Radom computer choice.
-  Random random = new Random();
-  int comp = random.nextInt(3);
-  //Generating user and comp ASCII art and result.
+            // To generate Radom computer choice.
+            Random random = new Random();
+            int comp = random.nextInt(3);
 
-  // Comp ASCII!
+            if(user == 9 ){
+                System.out.println("Exited....");
+                break;
+            }
 
-   switch (comp) {
-    case (0):
-        System.out.println(rock);
-        break;
-    case(1):
-        System.out.println(paper);
-    case(2):
-        System.out.println(scissors);
-   
-    default:
-        break;
-   }
+            //Generating user and comp ASCII art and result.
 
-   // User ASCII 
-   switch (user) {
-    case (0):
-    System.out.println(rock);
-        break;
-    case(1):
-        System.out.println(paper);
-        break;
-    case(2):
-        System.out.println(scissors);
-        break;
-    case (9): 
-        System.out.println("Exit");
-        break;
-   
-    default: 
-        System.out.println("You performed an invalid opearation.");
-        break;
-   }
-  
+            // Comp ASCII!
+            System.out.println("Computer choice:");
+            switch (comp) {
+                case (0):
+                    System.out.println(rock);
+                    break;
+                case(1):
+                    System.out.println(paper);
+                    break;
+                case(2):
+                    System.out.println(scissors);
+                default:
+                    break;
+            }
 
+            // User ASCII 
+            System.out.println("Your choice:");
+            switch (user) {
+                case (0):
+                    System.out.println(rock);
+                    break;
+                case(1):
+                    System.out.println(paper);
+                    break;
+                case(2):
+                    System.out.println(scissors);
+                    break;
+                case (9): 
+                    System.out.println("Exit");
+                    break;
+                default: 
+                    System.out.println("You performed an invalid opearation.");
+                    break;
+            }
 
-
-
+            // Main game logic.
+            if (user == 0 && comp == 0) {
+                System.out.println("AHH! It's a tie , Try again. ");
+            } else if (user == 0 && comp == 1){
+                System.out.println("Computer won! Better luck next time ;)");
+            } else if (user == 0 && comp == 2){
+                System.out.println("Bravo! You won.");
+            } else if (user == 1 && comp == 0){
+                System.out.println("Bravo! You won.");
+            } else if (user == 1 && comp == 1){
+                System.out.println("AHH! It's a tie , Try again.");
+            } else if (user == 1 && comp == 2){
+                System.out.println("Computer won! Better luck next time ;)");
+            } else if (user == 2 && comp == 0){
+                System.out.println("Computer won! Better luck next time ;)");
+            } else if (user == 2 && comp == 1){
+                System.out.println("Bravo! You won.");
+            } else if (user == 2 && comp == 2){
+                System.out.println("AHH! It's a tie , Try again.");
+            }
+        }
     }
 }
