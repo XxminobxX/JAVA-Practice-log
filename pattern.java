bb0d2964ab461221de2f546a6c5983b7f3df7b4e
yoyouuff <hardilshukla@gmail.com>
2026-01-07 18:28:23 +0000

    update

---
+++ pattern.java
@@ -0,0 +1,14 @@
+// creating a program to create a simple star pattern
+
+import java.util.Scanner;
+
+public class pattern {
+    public static void main(String[] args) {
+       int a = 5,b = 5;
+
+       for(int i=0;i<b;i++){
+        System.out.println("");
+       }
+
+    }
+}
