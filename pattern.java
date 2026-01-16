35676c9ed5baa0c42992baf99f847a62ef650690
yoyouuff <hardilshukla@gmail.com>
2026-01-16 10:47:18 +0000

    update

--- pattern.java
+++ pattern.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class pattern { 
+public class pattern {  
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
        while (true) { 
